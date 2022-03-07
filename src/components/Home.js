44b40fa4d9e9a19f25acd0e6f0b5cad5ad775c46
Kenny48620 <kennyleetrade@gmail.com>
2022-03-07 13:08:27 -0700

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image'
+
 
 
 const Home = () =>{
@@ -12,7 +12,7 @@ const Home = () =>{
 	var content = [];
 	flowers.map(flower=>{
 		content.push(<Col className='col-sm-3 p-3' style={{'text-align':'center' }}> 
-						<img  className='img-fluid' src={flower.picture}/>
+						<img  className='img-fluid' alt={flower.name} src={flower.picture}/>
 						<div> {flower.name}</div>
 						</Col>
 					  )
